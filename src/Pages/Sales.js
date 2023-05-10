a53c3a8c99792149cb26c4068363989d8c6e3f03
Wojtanello-coder <walonale@gmail.com>
2023-05-10 17:06:32 +0200

    Setting request

--- src/Pages/Sales.js
+++ src/Pages/Sales.js
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom";
 import Header from "../Components/Header";
+import List from "../Components/List";
 
 
 const Sales = () => {
 
     return(
         <div >
-            <List url='https://www.cheapshark.com/api/1.0/deals?storeID=1&sortBy=Price&pageSize=5'/>
-            <List url='https://www.cheapshark.com/api/1.0/deals?storeID=1&sortBy=Price&pageSize=5'/>
-            <List url='https://www.cheapshark.com/api/1.0/deals?storeID=1&sortBy=Price&pageSize=5'/>
+            <List url='https://www.cheapshark.com/api/1.0/deals?storeID=1&sortBy=Release&steamRating=80&pageSize=3'/>
+            <hr/>
+            <List url='https://www.cheapshark.com/api/1.0/deals?storeID=1&sortBy=Metacritic&onSale=1&pageSize=3'/>
+            <hr/>
+            <List url='https://www.cheapshark.com/api/1.0/deals?storeID=1&sortBy=Savings&AAA=1&pageSize=3'/>
             
 
 
